58557720dad4f90de0cf723c8966dcd8eb696716
Md. Shahadul Alam Patwary <shahadul_alam_17@outlook.com>
2018-12-06 00:53:56 +0600

    Updated

--- Project/Project/Search.cpp
+++ Project/Project/Search.cpp
@@ -108,112 +108,8 @@ int Search::bringFrontSearch(int query)		// based on most optimized linear searc
 	return index;
 }
 
-int Search::randomizedBringFrontSearch(int query)		// this performs worst...
-{
-	if (query == unsortedArray[windowLength])
-	{
-		return windowLength;
-	}
-
-	int i = 0, temporaryValue = unsortedArray[windowLength];
-	unsortedArray[windowLength] = query;
-
-	for ( ; ; i++)
-	{
-		if (query == unsortedArray[i])
-		{
-			unsortedArray[windowLength] = temporaryValue;
-
-			if (i < windowLength)
-			{
-				return i;
-			}
-
-			break;
-		}
-	}
-
-	int randomIndex = -1;
-	const int lastIndex = arrayLength - 1;
-
-	if (query == unsortedArray[lastIndex])
-	{
-		return lastIndex;
-	}
-	
-	temporaryValue = unsortedArray[lastIndex];
-	unsortedArray[lastIndex] = query;
-
-	for ( ; ; i++)
-	{
-		randomIndex = generateRandomNumber(i, lastIndex);
-
-		if (query == unsortedArray[randomIndex])
-		{
-			unsortedArray[lastIndex] = temporaryValue;
-
-			if (randomIndex < lastIndex)
-			{
-				if (randomIndex > windowLength)           // O (1)
-				{
-					/*
-					* if one element with same value as query already exists within the window...
-					* we should increment index of window by 1...
-					*/
-					while (query == unsortedArray[windowIndex])       // O (1)
-					{
-						circularIncrementWindowIndex();       // O (1)
-					}
-
-					// can be replaced by any kind of swap...
-					swapByXOR(unsortedArray + randomIndex, unsortedArray + windowIndex);
-
-					randomIndex = windowIndex;              // O (1)
-
-					circularIncrementWindowIndex();        // O (1)
-				}
-
-				return randomIndex;
-			}
-
-			return -1;
-		}
-		else
-        {
-            swapByXOR(unsortedArray + i, unsortedArray + randomIndex);
-        }
-	}
-}
-
-int Search::bringFrontSearch(bool randomized, int query)
-{
-	if (randomized)
-	{
-		return randomizedBringFrontSearch(query);
-	}
-	else
-	{
-		return bringFrontSearch(query);
-	}
-}
-
-void Search::print()
-{
-    for (int i = 0; i < arrayLength; i++)
-    {
-        cout << unsortedArray[i] << ", ";
-    }
-
-    cout << endl << endl;
-}
-
 void Search::swap(int *firstElement, int *secondElement)
 {
-	/*if (*firstElement == *secondElement)
-	{
-		return;
-	}*/
-
 	int temporaryValue = *firstElement;
 	*firstElement = *secondElement;
 	*secondElement = temporaryValue;
@@ -221,11 +117,6 @@ void Search::swap(int *firstElement, int *secondElement)
 
 void Search::swapByAddition(int *firstElement, int *secondElement)
 {
-	/*if (*firstElement == *secondElement)
-	{
-		return;
-	}*/
-
 	*firstElement += *secondElement;
 	*secondElement = *firstElement - *secondElement;
 	*firstElement -= *secondElement;
@@ -233,11 +124,6 @@ void Search::swapByAddition(int *firstElement, int *secondElement)
 
 void Search::swapByXOR(int *firstElement, int *secondElement)
 {
-	/*if (*firstElement == *secondElement)
-	{
-		return;
-	}*/
-
 	*firstElement ^= *secondElement;
 	*secondElement ^= *firstElement;
 	*firstElement ^= *secondElement;
--- Project/Project/Search.h
+++ Project/Project/Search.h
@@ -12,17 +12,15 @@ public:
 	void setUnsortedArray(int, int *);
 	void reset();
 	int linearSearch(int);
-	int bringFrontSearch(bool, int);
-	void print();
+	int bringFrontSearch(int);
 
 	static void setRandomNumberSeed();
 	static int generateRandomNumber(int, int);
 
 private:
-    int temporaryValue, windowIndex, windowLength, arrayLength, *unsortedArray, *initialUnsortedArray;
+    int windowIndex, windowLength, arrayLength, *unsortedArray, *initialUnsortedArray;
+
     void circularIncrementWindowIndex();
-	int bringFrontSearch(int);
-    int randomizedBringFrontSearch(int);
 
 	static void swap(int *, int *);
 	static void swapByAddition(int *, int *);
